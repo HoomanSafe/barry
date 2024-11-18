62c2fc44650c53ba2ea32c8a7b04a6df15e6ca44
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-18 18:47:39 +0800

    Update Des.jsx

--- src/Components/Des.jsx
+++ src/Components/Des.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import logo4 from "../assets/logo4.svg";
-import mics from "../assets/mics.svg";
+import logo4 from "../assets/logo1.jpg";
+import mics from "../assets/vid.mp4";
 
 function Des() {
   return (
@@ -11,28 +11,28 @@ function Des() {
           Hold The Line{" "}
         </h1>
         <p className="max-w-[700px] text-start lg:text-center text-black text-base font-normal font-['Jost'] leading-loose tracking-tight">
-          As the market goes in “depression” stage SBF has rumored of firing up
-          a new and improved FTX platform. <br />
+          Starting Barry inu's mission on the Binance Smart Chain network.
+          <br />
           <br />
           With due diligence done and brain power used… we still stand by
-          #FuckFTX
+          #BarryInu
         </p>
         <div
           onClick={() => {
             new Navigator().clipboard.writeText(
-              "0xd140e43C3e98883646Fb7255F8F65188f37F000e"
+              "0x0000000000000000"
             );
           }}
           className="w-full h-16 px-8 py-5 text-xl text-white font-semibold origin-top-left rotate-3 bg-[#F75F33] justify-center items-center gap-2.5 inline-flex"
         >
-          Contract: 0xd140.......{" "}
+          Contract: 0x000000.......{" "}
         </div>
 
         <div className="mt-16 grid lg:grid-cols-2 justify-between items-center gap-16">
           <div className="flex flex-col gap-8">
             <img src={mics} alt="" />
             <h1 className="text-orange-600 text-2xl font-normal font-JackCon">
-              Total Supply: 3,400,000,000
+              Total Supply: 1,000,000,000
             </h1>
           </div>
           <article className="grid place-items-center font-Jack">
@@ -44,7 +44,7 @@ function Des() {
                   </div>
                   <div className="justify-start items-center gap-3 flex">
                     <div className="text-black text-2xl font-semibold font-['Jost'] leading-loose">
-                      1/1
+                      5/5
                     </div>
                   </div>
                 </div>
@@ -52,11 +52,11 @@ function Des() {
               <div className="px-7 py-7 bg-orange-600 rounded-lg flex-col justify-start items-start gap-2.5 flex">
                 <div className="justify-start items-center gap-11 inline-flex">
                   <div className="text-black text-xl font-normal font-['Jack's Candlestick'] leading-7">
-                    Max Txn
+                    Dev wallet
                   </div>
                   <div className="justify-start items-center gap-3 flex">
                     <div className="text-black text-2xl font-semibold font-['Jost'] leading-loose">
-                      2%
+                      1%
                     </div>
                   </div>
                 </div>
